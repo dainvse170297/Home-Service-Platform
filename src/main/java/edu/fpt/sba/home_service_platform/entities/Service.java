dd0fe9b681864324632f3f3add96c24296bac7e5
hailinh2910 <linhnhse170530@fpt.edu.vn>
2025-01-18 19:48:07 +0700

    hello world

---
+++ src/main/java/edu/fpt/sba/home_service_platform/entities/Service.java
@@ -0,0 +1,23 @@
+package edu.fpt.sba.home_service_platform.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Service {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    String name;
+    String description;
+
+    @ManyToOne
+    Category category;
+}
