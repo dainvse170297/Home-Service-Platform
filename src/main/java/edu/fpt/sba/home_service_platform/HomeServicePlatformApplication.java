c35ee0e545cf060695a2f277d935242253faebce
hailinh03 <hailinh2910203@gmail.com>
2025-01-19 20:28:52 +0700

    entity

---
+++ src/main/java/edu/fpt/sba/home_service_platform/HomeServicePlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.fpt.sba.home_service_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServicePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServicePlatformApplication.class, args);
+	}
+
+}
